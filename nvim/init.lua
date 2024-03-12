891e0e09cd9269555c8a778b8bd2e4f0fd4ff5a6
ryanlupague <ryan.lupague@indebted.co>
2024-03-12 21:35:38 +0800

    first commit

---
+++ nvim/init.lua
@@ -0,0 +1,11 @@
+-- initializing core files
+require("alphire.core.keymaps")
+require("alphire.core.options")
+
+-- intiializing lazy plugin manager
+require("alphire.lazy")
+
+-- Disabling the new comment line when creating file
+
+
+
