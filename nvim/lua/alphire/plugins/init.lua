a389f89259731b665cf87cefc065be9204c9e8a7
ryanlupague <ryan.lupague@indebted.co>
2024-03-22 14:46:34 +0800

    move tmux navigator

--- nvim/lua/alphire/plugins/init.lua
+++ nvim/lua/alphire/plugins/init.lua
@@ -1,11 +1,8 @@
-return{
-    -- Navigating Split Windows like tmux
-    "christoomey/vim-tmux-navigator",
-    -- Maximizing Windows
-    "szw/vim-maximizer",
-    -- Plenary to use different functions
-    'nvim-lua/plenary.nvim',
-    -- To fix the delete buffer issue redirecting to nvimtree
-    'famiu/bufdelete.nvim',
+return {
+	-- Maximizing Windows
+	"szw/vim-maximizer",
+	-- Plenary to use different functions
+	"nvim-lua/plenary.nvim",
+	-- To fix the delete buffer issue redirecting to nvimtree
+	"famiu/bufdelete.nvim",
 }
-
