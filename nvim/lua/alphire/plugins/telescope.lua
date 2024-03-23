89d2c741ecebc7ddb1a02ea1fcacb63778d09da2
ryanlupague <ryan.lupague@indebted.co>
2024-03-23 08:32:23 +0800

    update telescope config

--- nvim/lua/alphire/plugins/telescope.lua
+++ nvim/lua/alphire/plugins/telescope.lua
@@ -1,5 +1,41 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.5',
--- or                              , branch = '0.1.x',
-      dependencies = { 'nvim-lua/plenary.nvim' },
+	"nvim-telescope/telescope.nvim",
+	tag = "0.1.5",
+	-- or                              , branch = '0.1.x',
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		require("telescope").setup({
+			defaults = {
+				-- Default configuration for telescope goes here:
+				-- config_key = value,
+				mappings = {
+					i = {
+						-- map actions.which_key to <C-h> (default: <C-/>)
+						-- actions.which_key shows the mappings for your picker,
+						-- e.g. git_{create, delete, ...}_branch for the git_branches picker
+						["<C-h>"] = "which_key",
+					},
+				},
+			},
+			pickers = {
+				find_files = {
+					find_command = {
+						"fd",
+						"--type",
+						"f",
+						"--color=never",
+						"--hidden",
+						"--follow",
+					},
+				},
+			},
+			extensions = {
+				-- Your extension configuration goes here:
+				-- extension_name = {
+				--   extension_config_key = value,
+				-- }
+				-- please take a look at the readme of the extension you want to configure
+			},
+		})
+	end,
 }
