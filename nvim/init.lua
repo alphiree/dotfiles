e921c094bb9149ef1e720ea21015f58a14a4f9f7
ryanlupague <ryan.lupague@indebted.co>
2024-03-23 08:16:35 +0800

    Add venv selector plugin
    Add this plugin to support and fix the pyright import issues

--- nvim/init.lua
+++ nvim/init.lua
@@ -7,5 +7,15 @@ require("alphire.lazy")
 
 -- Disabling the new comment line when creating file
 
-
-
+-- Initializing the venv from pyproject.toml in your workspace
+vim.api.nvim_create_autocmd("VimEnter", {
+	desc = "Auto select virtualenv Nvim open",
+	pattern = "*",
+	callback = function()
+		local venv = vim.fn.findfile("pyproject.toml", vim.fn.getcwd() .. ";")
+		if venv ~= "" then
+			require("venv-selector").retrieve_from_cache()
+		end
+	end,
+	once = true,
+})
