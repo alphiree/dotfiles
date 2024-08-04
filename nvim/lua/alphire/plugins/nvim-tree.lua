f54eda977fd55f5964343f3a522188d7047b08b4
alphire <rylupague@gmail.com>
2024-08-04 12:01:58 +0800

    update nvim tree config

--- nvim/lua/alphire/plugins/nvim-tree.lua
+++ nvim/lua/alphire/plugins/nvim-tree.lua
@@ -12,8 +12,8 @@ return {
 		vim.opt.termguicolors = true
 
 		-- change color for arrows in tree to light blue
-		-- vim.cmd([[ highlight NvimTreeFolderArrowClosed guifg=#3FC5FF ]])
-		-- vim.cmd([[ highlight NvimTreeFolderArrowOpen guifg=#3FC5FF ]])
+		vim.cmd([[ highlight NvimTreeFolderArrowClosed guifg=#ffb86c ]])
+		vim.cmd([[ highlight NvimTreeFolderArrowOpen guifg=#ffb86c ]])
 
 		-- configure nvim-tree
 		nvimtree.setup({
@@ -29,8 +29,8 @@ return {
 				icons = {
 					glyphs = {
 						folder = {
-							arrow_closed = "", -- arrow when folder is closed
-							arrow_open = "", -- arrow when folder is open
+							arrow_closed = ">", -- arrow when folder is closed
+							arrow_open = "v", -- arrow when folder is open
 						},
 					},
 				},
