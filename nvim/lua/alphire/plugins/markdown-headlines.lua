a1adf5c118b6ad75d78021d1dfb243a843e76bda
alphire <rylupague@gmail.com>
2024-08-23 20:27:03 +0800

    update markdown headlines

--- nvim/lua/alphire/plugins/markdown-headlines.lua
+++ nvim/lua/alphire/plugins/markdown-headlines.lua
@@ -3,11 +3,12 @@ return {
 	version = "*",
 	dependencies = "nvim-treesitter/nvim-treesitter",
 	config = function()
+		vim.cmd([[highlight Headline guibg=#201c1c]])
 		vim.cmd([[highlight CodeBlock guibg=#201c1c]])
-		vim.cmd([[highlight Quote guibg=#201c1c]])
+		vim.cmd([[highlight Quote guibg=NONE guifg=#201c1c]])
 		require("headlines").setup({
 			markdown = {
-				headline_highlights = { "Headline" },
+				headline_highlights = { "NONE" },
 				bullet_highlights = {
 					"@text.title.1.marker.markdown",
 					"@text.title.2.marker.markdown",
@@ -20,6 +21,7 @@ return {
 				codeblock_highlight = "CodeBlock",
 				dash_highlight = "Dash",
 				dash_string = "-",
+				backtick_highlight = "CodeBlock",
 				quote_highlight = "Quote",
 				quote_string = "┃",
 				fat_headlines = false,
