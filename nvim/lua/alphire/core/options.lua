3760faa033f2e4ec0550488ffec07aff376a7893
alphire <rylupague@gmail.com>
2024-09-07 10:40:01 +0800

    add highlight when yanking text

--- nvim/lua/alphire/core/options.lua
+++ nvim/lua/alphire/core/options.lua
@@ -58,3 +58,14 @@ vim.api.nvim_create_autocmd("BufEnter", {
 	group = vim.api.nvim_create_augroup("General", { clear = true }),
 	desc = "Disable New Line Comment",
 })
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
