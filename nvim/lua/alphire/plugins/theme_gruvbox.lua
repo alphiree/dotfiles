7e4ffc28419d543236aee865d9fbb2c38bd17645
alphire <rylupague@gmail.com>
2024-09-20 17:11:31 +0800

    update files

---
+++ nvim/lua/alphire/plugins/theme_gruvbox.lua
@@ -0,0 +1,12 @@
+return {
+	-- "ellisonleao/gruvbox.nvim",
+	-- -- priority = 1000,
+	-- config = function()
+	-- 	require("gruvbox").setup({
+	-- 		palette_overrides = {
+	-- 			bright_green = "#990000",
+	-- 		},
+	-- 	})
+	-- 	vim.cmd([[colorscheme gruvbox]])
+	-- end,
+}
