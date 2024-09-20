97ee8b809b9af54efd4aa4113c8edf279cdb90c6
dotfiles <dotfiles@example.invalid>
2024-09-20 17:11:31 +0800

    update files

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,17 +4,3 @@ require("alphire.core.options")
 
 -- intiializing lazy plugin manager
 require("alphire.lazy")
-
--- Initializing the venv from pyproject.toml in your workspace
-vim.api.nvim_create_autocmd("VimEnter", {
-	desc = "Auto select virtualenv Nvim open",
-	pattern = "*",
-	callback = function()
-		local pyproject = vim.fn.findfile("pyproject.toml", vim.fn.getcwd() .. ";")
-		local pipfile = vim.fn.findfile("Pipfile.lock", vim.fn.getcwd() .. ";")
-		if pyproject ~= "" or pipfile ~= "" then
-			require("venv-selector").retrieve_from_cache()
-		end
-	end,
-	once = true,
-})
