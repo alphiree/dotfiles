9a269f5a56c5293079a6715a4e8dd4384581272a
ryanlupague <ryan.lupague@indebted.co>
2024-10-09 17:44:49 +0800

    add the absolute path in lualine

--- nvim/lua/alphire/plugins/lualine.lua
+++ nvim/lua/alphire/plugins/lualine.lua
@@ -1,15 +1,20 @@
 return {
-    "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-        local auto_theme_custom = require('lualine.themes.monokai-pro')
-        auto_theme_custom.normal.c.bg = 'none'
-        require('lualine').setup {
-            options = {
-                -- theme = "catppuccin"
-                theme = auto_theme_custom
-                -- ... the rest of your lualine config
-            }
-        }
-    end,
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		local auto_theme_custom = require("lualine.themes.monokai-pro")
+		auto_theme_custom.normal.c.bg = "none"
+		require("lualine").setup({
+			options = {
+				-- theme = "catppuccin"
+				theme = auto_theme_custom,
+				-- ... the rest of your lualine config
+			},
+			sections = {
+				lualine_c = {
+					{ "filename", path = 1 },
+				},
+			},
+		})
+	end,
 }
