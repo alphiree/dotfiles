feb3b4994969a59fa650f57977396338ed224fb8
alphire <rylupague@gmail.com>
2024-10-12 19:20:57 +0800

    update to discard monokai

--- nvim/lua/alphire/plugins/theme_monokaipro.lua
+++ nvim/lua/alphire/plugins/theme_monokaipro.lua
@@ -2,7 +2,7 @@ return {
 	"loctvl842/monokai-pro.nvim",
 	config = function()
 		require("monokai-pro").setup({
-			transparent_background = true,
+			transparent_background = false,
 			terminal_colors = true,
 			devicons = true, -- highlight the icons of `nvim-web-devicons`
 			styles = {
@@ -41,6 +41,6 @@ return {
 				},
 			},
 		})
-		vim.cmd([[colorscheme monokai-pro]])
+		-- vim.cmd([[colorscheme monokai-pro]])
 	end,
 }
