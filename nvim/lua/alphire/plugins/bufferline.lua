b84e6895d29e8505dc330f1745a0cd9e1a1541f4
ryanlupague <ryan.lupague@indebted.co>
2024-10-28 21:06:27 +0800

    remove the use of bufferline

--- nvim/lua/alphire/plugins/bufferline.lua
+++ nvim/lua/alphire/plugins/bufferline.lua
@@ -1,25 +1,25 @@
 return {
-    "akinsho/bufferline.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    version = "*",
-    after = "catppuccin",
-    event = "BufReadPre",
-    config = function()
-        local bufferline = require("bufferline")
-        bufferline.setup({
-            highlights = require("catppuccin.groups.integrations.bufferline").get(),
-            options = {
-                offsets = {
-                    {
-                        filetype = "NvimTree",
-                        text = "File Explorer",
-                        highlights = "Directory",
-                        separator = true,
-                    }
-                },
-               -- separator_style = "slant",
-               mode = "buffer",
-            }
-        })
-    end
+	-- "akinsho/bufferline.nvim",
+	-- dependencies = { "nvim-tree/nvim-web-devicons" },
+	-- version = "*",
+	-- after = "catppuccin",
+	-- event = "BufReadPre",
+	-- config = function()
+	--     local bufferline = require("bufferline")
+	--     bufferline.setup({
+	--         highlights = require("catppuccin.groups.integrations.bufferline").get(),
+	--         options = {
+	--             offsets = {
+	--                 {
+	--                     filetype = "NvimTree",
+	--                     text = "File Explorer",
+	--                     highlights = "Directory",
+	--                     separator = true,
+	--                 }
+	--             },
+	--            -- separator_style = "slant",
+	--            mode = "buffer",
+	--         }
+	--     })
+	-- end
 }
