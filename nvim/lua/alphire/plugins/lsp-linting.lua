15eb388b7bbc3a8745c73ad58d8c095bd908be5d
dotfiles <dotfiles@example.invalid>
2024-11-01 12:03:42 +0800

    add sql linter

--- nvim/lua/alphire/plugins/lsp-linting.lua
+++ nvim/lua/alphire/plugins/lsp-linting.lua
@@ -12,6 +12,7 @@ return {
 			typescriptreact = { "eslint_d" },
 			svelte = { "eslint_d" },
 			python = { "pylint" },
+			sql = { "sqlfluff" },
 		}
 
 		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
@@ -26,5 +27,15 @@ return {
 		vim.keymap.set("n", "<leader>l", function()
 			lint.try_lint()
 		end, { desc = "Trigger linting for current file" })
+
+		-- Configure diagnostics to show the source
+		vim.diagnostic.config({
+			virtual_text = {
+				source = true, -- Show the source of the diagnostic always
+			},
+			float = {
+				source = true, -- Show the source in floating windows as well
+			},
+		})
 	end,
 }
