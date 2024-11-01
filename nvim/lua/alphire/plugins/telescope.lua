bc6ccee6270de9c5b7a7fddfc88fa9fe9b719eb8
ryanlupague <ryan.lupague@indebted.co>
2024-11-01 15:30:31 +0800

    update telescope for opening multiple file

--- nvim/lua/alphire/plugins/telescope.lua
+++ nvim/lua/alphire/plugins/telescope.lua
@@ -4,6 +4,8 @@ return {
 	-- or                              , branch = '0.1.x',
 	dependencies = { "nvim-lua/plenary.nvim" },
 	config = function()
+		local action_state = require("telescope.actions.state")
+
 		require("telescope").setup({
 			defaults = {
 				-- Default configuration for telescope goes here:
@@ -17,6 +19,18 @@ return {
 					},
 					n = {
 						["d"] = require("telescope.actions").delete_buffer,
+						-- Adds multiple files in the background as buffer
+						["<C-b>"] = function(prompt_bufnr)
+							local picker = action_state.get_current_picker(prompt_bufnr)
+							local multi_selections = picker:get_multi_selection()
+
+							-- Do not close the picker; just open the selected files in the background
+							for _, entry in ipairs(multi_selections) do
+								vim.cmd("badd " .. entry.value) -- Add the file to the buffer list without switching
+							end
+							-- Print a confirmation message
+							print("Selected files have been opened in buffers.")
+						end,
 					},
 				},
 				file_ignore_patterns = {
