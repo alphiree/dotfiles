b1c22929e4adcf43e61bb8a213f03b458367fddc
ryanlupague <ryan.lupague@indebted.co>
2025-05-01 22:26:30 +0800

    update lualine to handle transparency

--- nvim/lua/alphire/plugins/lualine.lua
+++ nvim/lua/alphire/plugins/lualine.lua
@@ -2,14 +2,14 @@ return {
 	"nvim-lualine/lualine.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
-		-- local auto_theme_custom = require("lualine.themes.monokai-pro")
-		-- auto_theme_custom.normal.c.bg = "none"
+		local auto_theme_custom = require("lualine.themes.monokai-pro")
+		auto_theme_custom.normal.c.bg = "none"
 		require("lualine").setup({
-			-- options = {
-			-- 	theme = "catppuccin",
-			-- 	-- theme = auto_theme_custom,
-			-- 	-- ... the rest of your lualine config
-			-- },
+			options = {
+				-- theme = "catppuccin",
+				theme = auto_theme_custom,
+				-- ... the rest of your lualine config
+			},
 			sections = {
 				lualine_a = {
 					"mode",
