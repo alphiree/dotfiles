0a8911201af64d5bb6505b565a1fd1653f751c6c
ryanlupague <ryan.lupague@indebted.co>
2025-06-05 14:36:00 +0800

    update the init lua to use the neovim env for loading python

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,18 @@
--- initializing core files
+vim.g.python3_host_prog = "/Users/ryan/.pyenv/versions/neovim/bin/python"
+
+-- To load python files faster using pyenv-virtualenvs:
+-- pyenv virtualenv 3.10.7 neovim
+-- pyenv activate neovim
+-- pip install pynvim
+--
+-- pyenv which python
+--
+-- Then, paste the directory to the `python3_host_prog` above so it will use the python provider when starting python files
+
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_node_provider = 0
+
 require("alphire.core.keymaps")
 require("alphire.core.options")
-
--- intiializing lazy plugin manager
 require("alphire.lazy")
